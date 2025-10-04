5e3b7cb7cd1fd5bdc413bcc6a4e234da9956b7f4
Miguel <carriscarmiguel@gmail.com>
2025-10-04 14:24:52 -0300

    criação rotas e screens basicas

---
+++ front-end/colonia/src/screens/Observatorio.jsx
@@ -0,0 +1,8 @@
+export default function Observatorio() {
+  return (
+    <div className="screen">
+      <h1>🛰️ Observatório Marciano</h1>
+      <p>Monitoramento de estrelas, satélites e comunicação com a Terra.</p>
+    </div>
+  );
+}
